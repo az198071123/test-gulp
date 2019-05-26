530c011ad596329c7d5699b3a2acbfe5e4306633
az198071123 <az198071123@gmail.com>
2019-05-26 18:18:26 +0800

    add css concat by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require("gulp-rename");
+
 gulp.task('concat', function () {
     return gulp.src('./app/css/*.css')
         .pipe(concat('all.css'))
         .pipe(gulp.dest('./build/css/'));
 });
+
+gulp.task('default', ['concat']);
